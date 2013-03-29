6273e6bf3b799a177c48ff2388f501364db0c355
Piotr Mitros <pmitros@mitx.mit.edu>
2013-03-29 07:49:03 -0400

    pip install gets the files in the right place

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import find_packages
 
 setup(name='djeventstream', 
       version='0.0',
-      package_dir={'djeventstream': 'src'}, 
-      packages=find_packages('src'),
+      package_dir={'djeventstream.snshandler' : 'src/snshandler',
+                   'djeventstream.httphandler' : 'src/httphandler', 
+                   'djeventstream' : 'src/djeventstream'}, 
+      packages=['djeventstream', 'djeventstream.snshandler', 'djeventstream.httphandler'],
       author="Piotr Mitros", 
       license="AGPLv3, see LICENSE.txt")
