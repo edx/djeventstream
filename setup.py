b3937d2118ab2a2c1fe72252afac26929f3874dc
Piotr Mitros <pmitros@mitx.mit.edu>
2013-03-29 07:42:14 -0400

    Experimenting with find_packages

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 setup(name='djeventstream', 
       version='0.0',
       package_dir={'djeventstream': 'src'}, 
-      packages=['djeventstream'], 
+      packages=find_packages('src'),
       author="Piotr Mitros", 
       license="AGPLv3, see LICENSE.txt")
